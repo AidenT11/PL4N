3b74a48e3212bbe0ced6b2a343c1b996e7e536c7
Aiden Thomas <aiden.thomas@student.manchester.ac.uk>
2025-09-10 21:44:49 +0100

    Added maximise/restore button

--- PL4N App/MainWindow.xaml.cs
+++ PL4N App/MainWindow.xaml.cs
@@ -1,13 +1,35 @@
-﻿using System.Windows;
+﻿using System.Runtime.CompilerServices;
+using System.Windows;
+using System.ComponentModel;
 
 namespace PL4N_App
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
+            DataContext = this;
             InitializeComponent();
+            if (WindowState == WindowState.Normal)
+            {
+                MaximiseEmoji = "1";
+            }
+            else if (WindowState == WindowState.Maximized)
+            {
+                MaximiseEmoji = "2";
+            }
         }
+
+        private string maximiseEmoji;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string MaximiseEmoji
+        {
+            get { return maximiseEmoji; }
+            set { maximiseEmoji = value; }
+        }
+
 
         private void btnClose_Click(object sender, RoutedEventArgs e)
         {
@@ -17,6 +39,26 @@ namespace PL4N_App
         private void btnMinimise_Click(object sender, RoutedEventArgs e)
         {
             WindowState = WindowState.Minimized;
+        }
+
+        private void btnMaximise_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+                MaximiseEmoji = "1";
+            }
+            else
+            {
+                WindowState = WindowState.Maximized;
+                MaximiseEmoji = "2";
+            }
+            OnPropertyChanged("MaximiseEmoji");
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
